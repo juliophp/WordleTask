5620a4feab4c82ef045b9ca1861bdcbcc339f52a
Adeyemi Okunola <s371112@oslomet.no>
2025-06-12 15:40:36 +0200

    updated null check to use optional of nullable

--- src/main/java/org/wordle/core/impl/FileWordReader.java
+++ src/main/java/org/wordle/core/impl/FileWordReader.java
@@ -6,10 +6,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 import java.util.function.Function;
 import java.util.logging.Logger;
@@ -23,7 +21,7 @@ public class FileWordReader implements IWordReader {
 
     public FileWordReader(String fileName, Pattern wordValidationPattern) {
         try(InputStream input = getClass().getClassLoader().getResourceAsStream(fileName)){
-            if (input == null) { throw new IOException("Sorry, unable to find file " + fileName);}
+            Optional.ofNullable(input).orElseThrow(() -> new IOException("Sorry, unable to find file " + fileName));
             BufferedReader reader = new BufferedReader(new InputStreamReader(input));
             Function<String, Stream<String>> linesToWordStreamMapper = line -> Arrays.stream(line.split(","));
             wordList = reader.lines().flatMap(linesToWordStreamMapper)
